ee43e9ffb0421b6f1e5ed82cc3064ce31b3d7480
大川翔 <workbookwork7@gmail.com>
2022-10-30 17:27:55 -0700

    add serializeField setting to speed props

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]
+    float steerSpeed = 1f;
+
+    [SerializeField]
+    float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,7 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
