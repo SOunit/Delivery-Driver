355156324d9e31f24e3f757411af3ffd4fc9a67c
SOUnit <workbookwork7@gmail.com>
2022-10-31 19:34:13 -0700

    add deleting package logic

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,6 +6,9 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage;
 
+    [SerializeField]
+    float destroyDelay = 0.5f;
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("OnCollisionEnter2D");
@@ -13,10 +16,11 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             hasPackage = true;
             Debug.Log("Package Picked Up");
+            Destroy(other.gameObject, destroyDelay);
         }
 
         if (other.tag == "Customer" && hasPackage)
